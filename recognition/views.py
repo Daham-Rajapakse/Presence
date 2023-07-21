9c1547945c0261ac7c42d9a285fa4b36b14920ff
AabidhaRifky <108779028+AabidhaRifky@users.noreply.github.com>
2023-07-22 00:19:20 +0530

    Added face detected option to the video stream window
    Made an improvement to detect, capture and display the recognized face in another window.

--- recognition/views.py
+++ recognition/views.py
@@ -549,7 +549,30 @@ def mark_your_attendance_out(request):
                     print(pred, present[pred], count[pred])
                 cv2.putText(frame, str(person_name) + str(prob), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
                             (0, 255, 0), 1)
+                # Show the face detected message for 5 seconds (5000 milliseconds)
+                # Display the detected face with the name for 5 seconds
+                cv2.imshow("Detected Face - " + person_name, cv2.resize(face_aligned, (300, 300)))  # Resize the window to 300x300
+                cv2.waitKey(5000)
+                cv2.destroyWindow("Detected Face - " + person_name)
 
+                # Create a black background image with larger dimensions
+                background_color = (0, 0, 0)
+                background_image = np.full((150, 700, 3), background_color, dtype=np.uint8)
+
+                # Add text to the image
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                text = person_name.title() + " Your Attendance-Out is marked"  # Modify this text as needed
+                text_color = (255, 255, 255)  # White color for the text
+                text_size = cv2.getTextSize(text, font, 1, 2)[0]
+                text_x = (background_image.shape[1] - text_size[0]) // 2
+                text_y = (background_image.shape[0] + text_size[1]) // 3
+                cv2.putText(background_image, text, (text_x, text_y), font, 1, text_color, 2, cv2.LINE_AA)
+
+                # Display the image in a window for 5 seconds
+                cv2.imshow("Face Detected", background_image)
+                update_attendance_in_db_out(present)
+                cv2.waitKey(5000)
+                
             else:
                 person_name = "unknown"
                 cv2.putText(frame, str(person_name), (x + 6, y + h - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
